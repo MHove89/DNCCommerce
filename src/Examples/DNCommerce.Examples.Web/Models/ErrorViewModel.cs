ff1f03468dcfb5c9dfa2ee1d0e97aceb8cf866fd
Mathias Hove <mathiashove02@gmail.com>
2020-04-16 20:21:48 +0200

    added example as mvc starter mvc site

---
+++ src/Examples/DNCommerce.Examples.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DNCommerce.Examples.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
